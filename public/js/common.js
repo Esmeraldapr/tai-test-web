6271d3b20c6b6748cacc52d0a49dd238a9b05a0c
Esmeralda <esmeraldaparaiso7@gmail.com>
2026-08-21 09:05:59 +0200

    Añade "Teoría" al menú lateral

--- public/js/common.js
+++ public/js/common.js
@@ -64,6 +64,7 @@ async function cerrarSesion() {
 const NAV_ITEMS = [
   { href: "index.html", icono: "🏠", texto: "Dashboard" },
   { href: "tutorial.html", icono: "❔", texto: "Tutorial" },
+  { href: "teoria.html", icono: "📖", texto: "Teoría" },
   { href: "temas.html", icono: "📘", texto: "Repasar Tema" },
   { href: "practica.html", icono: "⚡", texto: "Practicar" },
   { href: "cuestionarios.html", icono: "📝", texto: "Cuestionarios" },
